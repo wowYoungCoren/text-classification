deaf446017285ed9a9c71dbfe9f817e23d4a2afe
linzh17 <15018279123@163.com>
2019-03-11 12:45:37 +0800

    ent && money && sport && travel

--- data_process/newspider/网易新闻/dataProcess.py
+++ data_process/newspider/网易新闻/dataProcess.py
@@ -2,7 +2,7 @@ import json
 import jieba.posseg as pseg
 from json.decoder import JSONDecodeError
 
-docs = open('sportsnews.txt')
+'''docs = open('sportsnews.txt')
 f2 = labels = open('sports_labels.txt','a')
 
 docs_lines = docs.read().split('\n')
@@ -28,4 +28,54 @@ for doc in docs_lines:
 
 f1 = open('sports_tests.txt','w')
 f1.write('\n'.join(train))
-f1.close()
+f1.close()'''
+
+class dataProcess():
+    def __init__(self,docname):
+        self.docname = docname
+        
+
+    def process(self):
+        train = []
+        labels =[] 
+        docs = open(self.docname)
+        
+
+        docs_lines = docs.read().split('\n')
+
+        train = []
+        for doc in docs_lines:
+            try:
+                text = json.loads(doc)['content']
+                label = json.loads(doc)['lable']
+                print(text)
+                if text == "" or text == None:
+                    continue
+                if len(text) > 100: 
+                    text = text[:100]
+                words = pseg.cut(text)
+                line0 = [] 
+                for w in words:
+                    if 'x' != w.flag:
+                        line0.append(w.word)
+                train.append(' '.join(line0))
+                labels.append(str(label)) 
+                #f2.write(str(label)+'\n')
+            except JSONDecodeError as e:
+                print(doc+'cuowu')
+
+        f1 = open('testdata/testdata.txt','a')
+        f2 = open('testdata/testlabel.txt','a')
+        f1.write('\n'.join(train))
+        f2.write('\n'.join(labels))
+        f1.close()
+        f2.close()
+
+entD = dataProcess('datasource/entnews.txt')
+entD.process()
+moneyD = dataProcess('datasource/moneynews.txt')
+moneyD.process()
+travleD = dataProcess('datasource/travlenews.txt')
+travleD.process()
+sportD = dataProcess('data/source/sportsnews.txt')
+sportD.process()
